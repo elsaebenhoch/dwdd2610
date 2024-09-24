87ae83e29666276b8bc232e10d9949ac9a53bc13
elsaebenhoch <143566980+elsaebenhoch@users.noreply.github.com>
2024-09-24 10:01:55 -0600

    Add files via upload

---
+++ autoDealer/data/customers.js
@@ -0,0 +1,6 @@
+export const customers = [
+  {  "name": "Ian Jackson", "review": "Motor Mall is amazing! They did an impeccable job communicating and on their service. The process was seamless."},
+  {   "name": "Phoebe Hart", "review": "I had the fastest and most professional oil change today. I'm glad to know my car's maintenance will always come first. Thanks for helping me keep my vehicle healthy."},
+  {   "name": "Jack Goodman", "review": "They did a great job diagnosing and fixing up my car! They went out of their way to minimize the labor costs of a big job. Definitely recommend as an honest and quality place to take your vehicle in!"},
+  {   "name": "Rachel Smith", "review": "They always take such good care of me and my car, no matter what the issue is. They are great at explaining everything and asking if I have any questions. I feel very confident when they work on my car that everything will be done the way it should!"}
+]

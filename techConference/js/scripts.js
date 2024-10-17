b7013a48f834b8b6a91858108ba044da6a70f586
elsaebenhoch <143566980+elsaebenhoch@users.noreply.github.com>
2024-10-16 20:48:30 -0600

    Delete techConference directory

--- techConference/js/scripts.js
+++
@@ -1,7 +0,0 @@
-const btnElement = document.querySelector('#hamburgerBtn')
-const navElement = document.querySelector('#primaryNav')
-
-btnElement.addEventListener('click', () => {
-    btnElement.classList.toggle('open')
-    navElement.classList.toggle('open')
-})

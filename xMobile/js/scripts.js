3bc56f6e5ca7a1a7f7bab8fc0f778651fc457c48
elsaebenhoch <143566980+elsaebenhoch@users.noreply.github.com>
2024-10-30 09:25:29 -0600

    Delete xMobile directory

--- xMobile/js/scripts.js
+++
@@ -1,7 +0,0 @@
-const btnElement = document.querySelector('#primaryNav > button')
-const navElement = document.querySelector('#primaryNav > ul')
-
-btnElement.addEventListener ('click', () => {
-    btnElement.classList.toggle('open')
-    navElement.classList.toggle('open')
-})
